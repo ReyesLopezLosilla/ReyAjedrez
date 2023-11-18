7f3ccb2d217ee96218930077649d31858186ff69
ReyesLopezLosilla <reyeslofish@gmail.com>
2023-11-18 19:03:19 +0100

    Añado getter y setter y validación

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Posicion.java
@@ -3,4 +3,33 @@ package org.iesalandalus.programacion.reyajedrez.modelo;
 public class Posicion {
     private int fila;
     private char columna;
+
+
+    public int getFila() {
+        return fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setFila(int fila) throws IllegalArgumentException {
+        if (fila <1 || fila >8) {
+            throw new IllegalArgumentException("La fila no puede ser menor que uno o mayor que ocho.");
+        }
+        else {
+            this.fila = fila;
+        }
+    }
+
+    public void setColumna(char columna) throws IllegalArgumentException {
+        if (columna <'a' || columna > 'h'){
+            throw new IllegalArgumentException("La columna no puede ser diferente de a-h");
+        }
+        else {
+            this.columna = columna;
+        }
+    }
+
+
 }
