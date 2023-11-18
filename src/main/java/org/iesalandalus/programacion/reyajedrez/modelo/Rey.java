65092da8935533e9342d87315ab085f89ae8f737
ReyesLopezLosilla <reyeslofish@gmail.com>
2023-11-18 19:42:22 +0100

    Creo métodos get y set y valido cuando es necesario

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Rey.java
@@ -4,4 +4,29 @@ public class Rey {
     private Color color;
     private Posicion posicion;
     private int totalMovimientos;
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Posicion getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(Posicion posicion) throws IllegalArgumentException {
+        this.posicion = posicion;
+    }
+
+    public int getTotalMovimientos() {
+        return totalMovimientos;
+    }
+
+    public void setTotalMovimientos(int totalMovimientos) {
+        this.totalMovimientos = totalMovimientos;
+    }
+
 }
