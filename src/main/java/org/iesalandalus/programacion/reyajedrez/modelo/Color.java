b9b03a62979c880ba9c7043ace4add3b66e95b2a
ReyesLopezLosilla <reyeslofish@gmail.com>
2023-11-18 18:47:25 +0100

    Creo constructor de Color

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Color.java
@@ -1,6 +1,14 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
 public enum Color {
-    BLANCO ("Blanco"), NEGRO ("Negro")
+    BLANCO ("Blanco"), NEGRO ("Negro");
     private String cadenaAMostrar;
+
+    public Color (String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+
+
+
 }
