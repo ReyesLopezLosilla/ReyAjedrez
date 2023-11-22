31ef35046d339be10abd0e490b8c06bbca9dc675
ReyesLopezLosilla <reyeslofish@gmail.com>
2023-11-22 20:53:59 +0100

    Creo el método elegirOpcionMenu

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reyajedrez.modelo;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
     private Consola(){}
     public static void mostrarMenu(){
@@ -8,6 +10,17 @@ public class Consola {
         System.out.println("3. Mover");
         System.out.println("4. Salir");
     }
+    public static int elegirOpcionMenu(){
+        int opcionElegida;
+        System.out.println("Por favor, introduzca un número entre 1 y 4 en función de la opción deseada");
+        opcionElegida = Entrada.entero();
+        while (opcionElegida <1 || opcionElegida >4){
+            System.out.println("Por favor, introduzca un número de opción válido");
+            opcionElegida = Entrada.entero();
+        }
+    return opcionElegida;
+    }
+
 
 
 }
