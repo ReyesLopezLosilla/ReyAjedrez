e9ac751b26c7d857494577dc9d38d0293689b4c4
ReyesLopezLosilla <reyeslofish@gmail.com>
2023-11-22 20:43:53 +0100

    Creo el método mostrarMenu con las opciones

--- src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/modelo/Consola.java
@@ -2,6 +2,12 @@ package org.iesalandalus.programacion.reyajedrez.modelo;
 
 public class Consola {
     private Consola(){}
+    public static void mostrarMenu(){
+        System.out.println("1. Crear rey por defecto");
+        System.out.println("2. Crear rey eligiendo el color");
+        System.out.println("3. Mover");
+        System.out.println("4. Salir");
+    }
 
 
 }
