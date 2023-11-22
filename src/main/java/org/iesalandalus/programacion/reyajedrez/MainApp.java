c211dd22b90011fb61535b384dc677d32010eeb3
ReyesLopezLosilla <reyeslofish@gmail.com>
2023-11-22 21:53:03 +0100

    Creo método crearReyDefecto

--- src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reyajedrez/MainApp.java
@@ -4,8 +4,33 @@ import org.iesalandalus.programacion.reyajedrez.modelo.Rey;
 
 public class MainApp {
     private Rey rey;
+    public void ejecutarOpcion (int opcion){
+        switch(opcion){
+            case 1:
+                rey = new Rey();
+                break;
+            case 2:
+                Consola.elegirOpcion();
+                break;
+            case 3:
+                Consola.mostrarMenuDirecciones();
+                Consola.elegirDireccion();
+                rey.mover();
+                break;
+            case 4:
+                Consola.despedirse();
+                break;
+        }
+
+
+    }
+    public void crearReyDefecto(){
+        rey = new Rey();
+    }
+
     public static void main(String[] args) {
 
 
     }
+
 }
